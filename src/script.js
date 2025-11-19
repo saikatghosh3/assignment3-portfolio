5af43c9bb7207819f07f91919f5077227cd82a13
Saikat Ghosh <saikat.blast20@gmail.com>
2025-11-20 03:36:19 +0600

    basic-frame

---
+++ src/script.js
@@ -0,0 +1,91 @@
+// Mobile menu toggle
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("mobileMenu");
+const iconOpen = document.getElementById("iconOpen");
+const iconClose = document.getElementById("iconClose");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  
+  // Toggle icons
+  iconOpen.classList.toggle("hidden");
+  iconClose.classList.toggle("hidden");
+});
+
+
+
+
+
+// Typing effect for dynamic title
+
+
+  const text = "Hi, I am Saikat Ghosh";
+  const typingSpeed = 80;     
+  const eraseSpeed = 40;      
+  const repeatTimes = 10;     
+
+  let index = 0;
+  let count = 0;
+  let isDeleting = false;
+
+  const title = document.getElementById("dynamicTitle");
+
+  function typeLoop() {
+    if (count >= repeatTimes) return; 
+
+    if (!isDeleting) {
+      title.innerHTML = text.slice(0, index);
+      index++;
+
+      if (index > text.length) {
+        isDeleting = true;
+        setTimeout(typeLoop, 1000); 
+        return;
+      }
+    } else {
+      title.innerHTML = text.slice(0, index);
+      index--;
+
+      if (index < 0) {
+        isDeleting = false;
+        count++;
+      }
+    }
+
+    const speed = isDeleting ? eraseSpeed : typingSpeed;
+    setTimeout(typeLoop, speed);
+  }
+
+  window.onload = typeLoop;
+
+
+
+
+
+
+
+  // scroll to top button script start
+ 
+  const scrollBtn = document.getElementById("scrollTopBtn");
+
+  // Show button after scrolling down 200px
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      scrollBtn.classList.remove("hidden");
+    } else {
+      scrollBtn.classList.add("hidden");
+    }
+  });
+
+  // Scroll to top smoothly
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+
+  
+
+  // scroll to top button script end  
